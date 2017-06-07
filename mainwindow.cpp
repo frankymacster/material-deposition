3093c768f2c7195a28e2f85d5aa1c5af38dfde3c
frankymacster <fs.sunatori@me.com>
2017-06-07 18:49:53 -0400

    print ui input on get Path click

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -11,4 +13,25 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    float upperRectWidth = ui->lineEdit->text().toFloat();
+    float upperRectLength = ui->lineEdit_2->text().toFloat();
+    float lowerRectWidth = ui->lineEdit_3->text().toFloat();
+    float lowerRectLength = ui->lineEdit_4->text().toFloat();
+    float overallPartHeight = ui->lineEdit_5->text().toFloat();
+    float stepUpHeight = ui->lineEdit_6->text().toFloat();
+    float radiusOfMaterial = ui->lineEdit_7->text().toFloat();
+    QString pathDirection = ui->comboBox->itemText(ui->comboBox->currentIndex());
+
+    qDebug() << upperRectWidth;
+    qDebug() << upperRectLength;
+    qDebug() << lowerRectWidth;
+    qDebug() << lowerRectLength;
+    qDebug() << overallPartHeight;
+    qDebug() << stepUpHeight;
+    qDebug() << radiusOfMaterial;
+    qDebug() << pathDirection;
 }
